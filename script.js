280fed6d3c13be27ec1180a937fed38826efc704
enayat789 <enayatbhai2006@gmail.com>
2023-08-04 17:37:58 +0530

    added footer with so many buttons

---
+++ script.js
@@ -0,0 +1,8 @@
+function searchFunction() {
+  let filter = document.getElementById("mySearch").value;
+
+  let myteamProfile = document.getElementsByClassName("team_profile");
+
+  let firstTeam = myteamProfile.getElementsByClassName("team1");
+  let myProfile = firstTeam.getElementsByClassName("profile1");
+}
